52e280e8f443ba630b91c192b6ef28722030ff31
Felipe Costa <costadefelipe@gmail.com>
2020-12-21 13:04:18 -0300

    exercicios

---
+++ programas/capitulo-02/calcular_expressoes.py
@@ -0,0 +1,26 @@
+# Calcular expressoes
+#1) 
+A = 1 
+B = 2
+C = True
+D = False
+
+print(A > B and C or D)
+
+#2)
+A = 10
+B = 3 
+C = False
+D = False
+
+result = A > B and C or D
+print result
+
+#3)
+A = 5
+B = 1
+C = True
+D = True
+
+result = A > B and C or D
+print("resultado", result)
