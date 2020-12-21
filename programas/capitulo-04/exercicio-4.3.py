bf3acf127b7a4bc85efcf02496b13f06e294de54
Felipe Costa <costadefelipe@gmail.com>
2020-12-21 18:18:13 -0300

    exercicio e listagem

---
+++ programas/capitulo-04/exercicio-4.3.py
@@ -0,0 +1,6 @@
+number01 = int(input("Enter a number:"))
+number02 = int(input("Enter another number:"))
+number03 = int(input("Enter a number:"))
+
+print("O maior numero é:", max(number01, number02, number03))
+print("O menor numero é:", min(number01, number02, number03))
