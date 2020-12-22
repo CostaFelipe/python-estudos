cfcf1cd8aaefcb36f78ee37ed51b52384faaa972
Felipe Costa <costadefelipe@gmail.com>
2020-12-22 14:08:59 -0300

    condicional while

---
+++ programas/capitulo-05/tabuada.py
@@ -0,0 +1,7 @@
+n = int(input("Tabuada de:"))
+x = 1
+
+while x <= n:
+    print("%d + %d" %(n, x),"=", n + x)
+    x = x + 1
+
---
+++ programas/capitulo-05/tabuadamult.py
@@ -0,0 +1,6 @@
+n = int(input("Digite a tabuada de um numero:"))
+x = 1
+
+while x <= 10:
+    print("%d x %d" %(n, x),"=", n * x)
+    x = x + 1
