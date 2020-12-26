d92deab8536b28108ae23b017b0d758ab4a4f16d
Felipe Costa <costadefelipe@gmail.com>
2020-12-26 13:20:36 -0300

    variable & data types

--- programas/Giraffe/init/app.py
+++ programas/Giraffe/init/app.py
@@ -1,3 +1,13 @@
 # hello world
 print("Hello World")
 
+print("\n")
+#variables & data types
+
+character_name = "Larissa"
+character_age  = 25
+
+print("There once is womem named",character_name)
+print("She have %d" %character_age, "years old.")
+print("She smart and beatiful, really like the name %s" %character_name)
+print("but did like being", character_age)
