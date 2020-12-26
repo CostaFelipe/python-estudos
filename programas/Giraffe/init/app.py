48b7035da9b4db3dc37781a8579707b41d4d4b2a
Felipe Costa <costadefelipe@gmail.com>
2020-12-26 13:34:32 -0300

    working with strings

--- programas/Giraffe/init/app.py
+++ programas/Giraffe/init/app.py
@@ -11,3 +11,19 @@ print("There once is womem named",character_name)
 print("She have %d" %character_age, "years old.")
 print("She smart and beatiful, really like the name %s" %character_name)
 print("but did like being", character_age)
+
+print("\n")
+
+#Working with strings
+phrase = "Martins Academy"
+print("Martins Academy")
+print("Martins\"Academy")
+print(phrase)
+print(phrase + " is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(len(phrase))
+print(phrase[3])
+print(phrase[0])
+print(phrase.index("A"))
+print(phrase.replace("Martins", "Lima"))
