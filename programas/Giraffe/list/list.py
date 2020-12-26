bb0bb729bb01cdf230252a8efe00f9e3f95dfcc3
Felipe Costa <costadefelipe@gmail.com>
2020-12-26 14:19:29 -0300

    basic list

---
+++ programas/Giraffe/list/list.py
@@ -0,0 +1,16 @@
+friends = ["Ruth", "Levi", "Diego"]
+print(friends)
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[-1])
+print(friends[-2])
+print(friends[-3])
+print(friends[1:3])
+print(friends[1:])
+print(friends[:2])
+
+friends[2] = "Mike"
+print(friends)
+
+#list functions
