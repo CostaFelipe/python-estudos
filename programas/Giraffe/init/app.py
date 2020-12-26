11d1e7e72e032c9fb7c648238993198a58dc8954
Felipe Costa <costadefelipe@gmail.com>
2020-12-26 13:50:21 -0300

    working with numbers

--- programas/Giraffe/init/app.py
+++ programas/Giraffe/init/app.py
@@ -1,3 +1,4 @@
+from math import  *
 # hello world
 print("Hello World")
 
@@ -12,7 +13,7 @@ print("She have %d" %character_age, "years old.")
 print("She smart and beatiful, really like the name %s" %character_name)
 print("but did like being", character_age)
 
-print("\n")
+print("\nStrings")
 
 #Working with strings
 phrase = "Martins Academy"
@@ -27,3 +28,29 @@ print(phrase[3])
 print(phrase[0])
 print(phrase.index("A"))
 print(phrase.replace("Martins", "Lima"))
+
+print("\nworking with Numbers")
+#working with numbers
+print(2)
+print(2.2)
+print(-2.73637)
+print(2 + 1)
+print(2 ** 2)
+print(2 * 5)
+print(10 / 2)
+print(2 - 6)
+print(10 % 3)
+print(10 // 3)
+
+my_num = 5
+print(my_num)
+print(str(my_num) + " My favorite number")
+print(abs(my_num))
+print(pow(3, 2))
+print(max(4, 2))
+print(min(88, 67))
+print(round(2.1))
+print(round(3.9))
+print(floor(3.7))
+print(ceil(6.5))
+print(sqrt(9))
