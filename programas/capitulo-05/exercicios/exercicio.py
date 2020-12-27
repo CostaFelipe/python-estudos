666dfd6a1a6a9dfd0b23dee3ca6cc81ee2144023
Felipe Costa <costadefelipe@gmail.com>
2020-12-27 16:13:39 -0300

    revisao capitulo 5

---
+++ programas/capitulo-05/exercicios/exercicio.py
@@ -0,0 +1,14 @@
+#n = int(input("Digite um numero:"))
+#x = 1
+
+#while x <= 10:
+#    print("%d x %d = %d" %(n, x, n * x))
+#    x+=1
+
+n   = int(input("Digite um numero para multiplicar:"))
+fim = int(input("Digite o fim da tabuada:"))
+x = 1
+
+while x <= fim:
+    print("%d x %d = %d" %(n, x, n * x))
+    x+=1
