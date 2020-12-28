c210dc95600a2ffab528664d6cc7af2cb4d6147b
Felipe Costa <costadefelipe@gmail.com>
2020-12-28 13:11:12 -0300

    add novo exercicio

---
+++ programas/capitulo-05/exercicios/exercicio_pontos.py
@@ -0,0 +1,13 @@
+pontos = 0
+questoes = 1
+
+while questoes <= 3:
+    resposta = input("Respostas da questão %d:" %questoes)
+    if questoes == 1 and resposta == "b":
+        pontos = pontos + 1
+    if questoes == 2 and resposta == "c":
+        pontos = pontos + 1
+    if questoes == 3 and resposta == "a":
+        pontos = pontos + 1
+    questoes+=1
+print("O aluno fez %d ponto(s)" %pontos)
