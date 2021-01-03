3d1e1c5af8c218b613f99568157abb6738d2af09
Felipe Costa <costadefelipe@gmail.com>
2021-01-03 19:51:42 -0300

    Maior da lista while

--- programas/Giraffe/list/exercises/03.py
+++ programas/Giraffe/list/exercises/03.py
@@ -1,1 +1,10 @@
 #3. Write a Python program to get the largest number from a list.
+numbers = [777, 443]
+x = 1
+maior = numbers[0]
+while x < len(numbers):
+   if numbers[x] > maior:
+       maior = numbers[x]
+   x+=1
+print(maior)
+
