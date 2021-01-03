436b481c944630e7028547c11652a6215cb3ea34
Felipe Costa <costadefelipe@gmail.com>
2021-01-03 14:32:50 -0300

    Listas

--- programas/Giraffe/list/list.py
+++ programas/Giraffe/list/list.py
@@ -14,3 +14,38 @@ friends[2] = "Mike"
 print(friends)
 
 #list functions
+luck_numbers = [111, 22, 3, 7, 8]
+friends = ["Ruth", "Levi", "Diego"]
+#friends.extend(luck_numbers)
+print(friends)
+friends.append("Linda")
+print(friends)
+
+friends.insert(1, "Esther")
+print(friends)
+
+friends.remove("Diego")
+print(friends)
+
+friends.clear()
+print(friends)
+
+friends = ["Kevin", "Karen", "Jim", "Oscar"]
+friends.pop()
+print(friends)
+print(friends.index("Jim"))
+
+friends = ["Kevin", "Karen", "Jim", "Jim", "Oscar", "Anne"]
+print(friends.count("Karen"))
+
+friends.sort()
+print(friends)
+
+luck_numbers.sort()
+print(luck_numbers)
+
+luck_numbers.reverse()
+print(luck_numbers)
+
+numbers = luck_numbers.copy()
+print(numbers)
