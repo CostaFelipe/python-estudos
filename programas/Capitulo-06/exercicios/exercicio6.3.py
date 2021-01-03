2cee355a4543ba81059bf017c96c94d962b671a4
Felipe Costa <costadefelipe@gmail.com>
2021-01-03 13:41:29 -0300

    exercicio

--- programas/Capitulo-06/exercicios/exercicio6.3.py
+++ programas/Capitulo-06/exercicios/exercicio6.3.py
@@ -1,1 +1,19 @@
+primeira = []
+segunda  = []
+x = 0
 
+while True:
+    n = int(input("Add numbers list 01:"))
+    if n == 0:
+        break
+    primeira.append(n)
+
+while True:
+    n = int(input("Add numbers list 02:"))
+    if n == 0:
+        break
+    segunda.append(n)
+
+terceira = primeira[:]
+terceira.extend(segunda)
+print(set(terceira))
