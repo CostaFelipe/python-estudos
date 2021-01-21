e8e6e536eb324c8f799ed96b6ec5c011dc040b18
Felipe Costa <costadefelipe@gmail.com>
2021-01-21 08:58:42 -0300

    method pop() and del

---
+++ programas/book_02/revisao_13.py
@@ -0,0 +1,8 @@
+motorcycles = ['honda', 'yamaha', 'susuki']
+del motorcycles[0]
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'susuki']
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
