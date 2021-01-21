3428c6820bcb2f02c0bd78be823ed3dcbbca4d89
Felipe Costa <costadefelipe@gmail.com>
2021-01-21 08:51:20 -0300

    listas append = add last list and insert add index 0 list

--- programas/book_02/revisao_11.py
+++ programas/book_02/revisao_11.py
@@ -4,3 +4,7 @@ print(bicycles[0])
 print(bicycles[1])
 print(bicycles[3])
 print(bicycles[-1])
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
---
+++ programas/book_02/revisao_12.py
@@ -0,0 +1,11 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
