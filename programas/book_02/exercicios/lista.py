21449e46f14a1707b77ac1dcd2e7eca78b16f243
Felipe Costa <costadefelipe@gmail.com>
2021-01-21 12:31:09 -0300

    exercicios listas parte 1

---
+++ programas/book_02/exercicios/lista.py
@@ -0,0 +1,9 @@
+friends_lists = ['Ruth', 'Levi', 'Sueli', 'Raimundo', 'Felipe']
+message = "Eu felipe convido-os ao jantar"
+print(message + " " + friends_lists[0])
+print(message + " " + friends_lists[1])
+print(message + " " + friends_lists[2])
+print(message + " " + friends_lists[3])
+print(message + " " + friends_lists[4])
+friends_lists[4] = 'Paulo'
+print(friends_lists)
