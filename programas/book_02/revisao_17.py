33fc07d492310d8334a96a1b9f51575fd9df8b1c
Felipe Costa <costadefelipe@gmail.com>
2021-01-23 14:18:34 -0300

    listas

---
+++ programas/book_02/revisao_17.py
@@ -0,0 +1,16 @@
+cars = ['bmw', 'toyota', 'honda', 'fiat']
+del cars[0]
+print(cars)
+
+cars.remove('toyota')
+print(cars)
+
+cars = ['bmw', 'toyota', 'honda', 'fiat']
+cars.pop()
+print(cars)
+
+cars.insert(1, 'ford')
+print(cars)
+
+cars.append("hyndai")
+print(cars)
