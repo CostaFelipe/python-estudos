a952f8dfbb94564032e62a9839adf4176e68450c
Felipe Costa <costadefelipe@gmail.com>
2021-01-23 14:31:24 -0300

    exercicio sort(), sorted(), reverse()

---
+++ programas/book_02/exercicios/sorted.py
@@ -0,0 +1,24 @@
+localidades = ['Polonia', 'Japão', 'Santa Catarina', 'Israel']
+print(localidades)
+
+print(sorted(localidades))
+print(localidades)
+
+print('reverse sorted:', sorted(localidades, reverse=True))
+print(localidades)
+
+localidades = ['Polonia', 'Japão', 'Santa Catarina', 'Israel']
+localidades.reverse()
+print('reverse metodo:', localidades)
+
+localidades.reverse()
+print(localidades)
+
+localidades = ['Polonia', 'Japão', 'Santa Catarina', 'Israel']
+localidades.sort()
+print('sort method',localidades)
+
+localidades = ['Polonia', 'Japão', 'Santa Catarina', 'Israel']
+localidades.sort(reverse=True)
+print('sort reverse=True', localidades)
+
