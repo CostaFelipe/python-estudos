e71ab1ab1c78d13942c7799006766f12b485fd0e
Felipe Costa <costadefelipe@gmail.com>
2021-01-26 12:41:04 -0300

    slicing

--- programas/book_02/revisao_18.py
+++ programas/book_02/revisao_18.py
@@ -5,3 +5,8 @@ Avengers = ['Hulk', 'Iron-Man', 'Captain', 'Thor']
 a, b = [1, 2]
 print(a)
 print(b)
+
+Avengers = ['Hulk', 'Iron-Man', 'Captain', 'Thor']
+print(Avengers[0])
+print(Avengers[1])
+print(Avengers[1:3])
