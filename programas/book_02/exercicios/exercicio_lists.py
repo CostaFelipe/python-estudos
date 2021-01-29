627a944d92f52214399285d8c1fec9678f6b816d
Felipe Costa <costadefelipe@gmail.com>
2021-01-29 17:48:55 -0300

    exercicio lista 1

--- programas/book_02/exercicios/exercicio_lists.py
+++ programas/book_02/exercicios/exercicio_lists.py
@@ -1,8 +1,3 @@
 # string == avengers
-
 list1 = ["abc",[2,3,("mohit","the avengers")],1,2,3]
-list2 = []
-
-for i in range(len(list1)):
-    if list1[i] == "abc":
-        print(list1[i])
+print(list1[1][2][1][4:])
