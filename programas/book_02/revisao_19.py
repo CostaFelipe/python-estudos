dbf3573dfc90202b5af27115f293e0fc01c9c011
Felipe Costa <costadefelipe@gmail.com>
2021-01-31 14:56:21 -0300

    lists numbers perfects

--- programas/book_02/revisao_19.py
+++ programas/book_02/revisao_19.py
@@ -15,3 +15,10 @@ print(numbers)
 
 even_numbers = list(range(2, 11, 2))
 print(even_numbers)
+
+#numeros perfeitos
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+    print(squares)
