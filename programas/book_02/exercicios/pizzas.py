ae1be028f2e06abe6dd7ee31a3015296cd7cd526
Felipe Costa <costadefelipe@gmail.com>
2021-01-31 14:45:01 -0300

    lists animals

--- programas/book_02/exercicios/pizzas.py
+++ programas/book_02/exercicios/pizzas.py
@@ -3,4 +3,12 @@ pizzas = ['portuguesa', 'calabreza', 'frango capituri']
 for pizza in pizzas:
     print("Gosto da pizza,", pizza)
 
-print("Eu realmente gosto de pizza!)
+print("Eu realmente gosto de pizza!")
+
+print("\n")
+
+animals = ["Cachorro", "Elefante", "aguia-americana", "cavalo"]
+for animal in animals:
+    print(animal + ", é um dos meus animais favoritos.")
+
+print("Qualquer um desses animais seria um ótimo animal de estimação!")
