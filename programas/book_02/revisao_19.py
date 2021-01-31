5c00da06ac296ef61a5ce8209601a7678ad8a1fe
Felipe Costa <costadefelipe@gmail.com>
2021-01-31 15:00:27 -0300

    lists squares

--- programas/book_02/revisao_19.py
+++ programas/book_02/revisao_19.py
@@ -22,3 +22,10 @@ for value in range(1, 11):
     square = value ** 2
     squares.append(square)
     print(squares)
+
+print("oi\n")
+
+#numeros perfeitos mais enxuto
+for value in range(1, 11):
+    squares.append(value ** 2)
+    print(squares)
