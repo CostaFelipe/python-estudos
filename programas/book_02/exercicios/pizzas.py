72dd5e4ba8fa952c7494eeb0b463ac3758de23dc
Felipe Costa <costadefelipe@gmail.com>
2021-01-31 14:40:34 -0300

    lists pizzas

---
+++ programas/book_02/exercicios/pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['portuguesa', 'calabreza', 'frango capituri']
+
+for pizza in pizzas:
+    print("Gosto da pizza,", pizza)
+
+print("Eu realmente gosto de pizza!)
--- programas/book_02/revisao_19.py
+++ programas/book_02/revisao_19.py
@@ -4,8 +4,7 @@ for magician in magicians:
     print(magician.title() + ", that was a great trick!")
     print("I can't wait to see your next trick, " + magician.title(), "\n")
 
-for magician in magicians:
-    print("I can't wait to see your next trick, " + magician.title())
+print("Thank you, everyone. That was a great magic show!") 
 
 
 
