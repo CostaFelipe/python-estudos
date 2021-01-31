9e0e818ed17e1ba058357d75730c9c828d44da76
Felipe Costa <costadefelipe@gmail.com>
2021-01-31 14:52:04 -0300

    função range()

--- programas/book_02/revisao_19.py
+++ programas/book_02/revisao_19.py
@@ -6,5 +6,12 @@ for magician in magicians:
 
 print("Thank you, everyone. That was a great magic show!") 
 
+for value in range(1, 5):
+    print(value)
 
+#converter range in lists
+numbers = list(range(1,6)) 
+print(numbers)
 
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
