bcb122a04b4160c4738130cd99701d71c631d699
Felipe Costa <costadefelipe@gmail.com>
2021-01-31 14:27:14 -0300

    lists magicians

--- programas/book_02/revisao_19.py
+++ programas/book_02/revisao_19.py
@@ -1,5 +1,11 @@
 magicians = ['alice', 'david', 'carolina']
 
 for magician in magicians:
-    print(magician)
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title(), "\n")
+
+for magician in magicians:
+    print("I can't wait to see your next trick, " + magician.title())
+
+
 
