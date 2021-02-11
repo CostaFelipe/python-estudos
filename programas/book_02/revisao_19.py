25d3735c6ca6be2af76c5f9ad34ad6202e902d1f
Felipe Costa <costadefelipe@gmail.com>
2021-02-11 14:24:00 -0300

    quadrado perfeito

--- programas/book_02/revisao_19.py
+++ programas/book_02/revisao_19.py
@@ -1,56 +1,5 @@
-magicians = ['alice', 'david', 'carolina']
-
-for magician in magicians:
-    print(magician.title() + ", that was a great trick!")
-    print("I can't wait to see your next trick, " + magician.title(), "\n")
-
-print("Thank you, everyone. That was a great magic show!") 
-
-for value in range(1, 5):
-    print(value)
-
-#converter range in lists
-numbers = list(range(1,6)) 
-print(numbers)
-
-even_numbers = list(range(2, 11, 2))
-print(even_numbers)
-
-#numeros perfeitos
 squares = []
-for value in range(1, 11):
-    square = value ** 2
+for values in range(1, 11):
+    square = values**2
     squares.append(square)
     print(squares)
-
-print("oi\n")
-
-#numeros perfeitos mais enxuto
-for value in range(1, 11):
-    squares.append(value ** 2)
-    print(squares)
-
-#max() min() sum()
-digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-print(min(digits))
-print(max(digits))
-print(sum(digits))
-
-# lists comprehensions
-square = [value ** 2 for value in range(1, 11)]
-print(square)
-
-even_numbers = [value for value in range(2, 11, 2)]
-print(even_numbers)
-
-multiplos = []
-
-for tres in range(1, 31):
-    if tres % 3 == 0:
-        multiplos.append(tres)
-
-print(multiplos)
-
-multiplos_tres = [value for value in range(3, 31, 3)]
-print(multiplos_tres)
-
