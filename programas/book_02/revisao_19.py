6cc0b8c1c313efe79bda5b12d8958c408db61ca8
Felipe Costa <costadefelipe@gmail.com>
2021-02-11 14:26:41 -0300

    list(range(1,11)

--- programas/book_02/revisao_19.py
+++ programas/book_02/revisao_19.py
@@ -3,3 +3,9 @@ for values in range(1, 11):
     square = values**2
     squares.append(square)
     print(squares)
+
+for values in range(1,6):
+    print(values)
+
+numbers = list(range(1, 11))
+print(numbers)
