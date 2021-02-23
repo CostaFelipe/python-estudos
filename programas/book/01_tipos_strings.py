6b70f8ddda28ab35c590f31d46280bcb9cf3e728
Felipe Costa <costadefelipe@gmail.com>
2021-02-23 14:57:08 -0300

    letras maiusculas e minusculas

--- programas/book/01_tipos_strings.py
+++ programas/book/01_tipos_strings.py
@@ -3,3 +3,13 @@ print('this is a string')
 
 message = 'I told my friends "Python is my favorite language"'
 print(message)
+
+#letras maiusculas e minusculas
+name = "ada lovelace"
+print(name.title())
+
+name = "ADA LOVELACE"
+print(name.lower())
+
+name = "Ada Lovelace"
+print(name.upper())
