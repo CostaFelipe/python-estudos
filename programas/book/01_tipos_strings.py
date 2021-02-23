ae5fb5b07ed824aa306c188964157fdc168eed77
Felipe Costa <costadefelipe@gmail.com>
2021-02-23 14:59:52 -0300

    Combinando ou concatenando strings

--- programas/book/01_tipos_strings.py
+++ programas/book/01_tipos_strings.py
@@ -14,7 +14,7 @@ print(name.lower())
 name = "Ada Lovelace"
 print(name.upper())
 
-#Combinando ou concatenando stringss
+#Combinando ou concatenando strings
 first_name = "Ada"
 last_name = "Lovelace"
 print(first_name + " " + last_name)
