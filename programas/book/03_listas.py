9b7e6f22f03cf12d40b0302d3c3f0ed975be0a9e
Felipe Costa <costadefelipe@gmail.com>
2021-02-25 13:49:19 -0300

    position index

--- programas/book/03_listas.py
+++ programas/book/03_listas.py
@@ -2,3 +2,12 @@
 
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+
+#acesso os elementos da listas
+print(bicycles[0])
+print(bicycles[0].title())
+
+print(bicycles[3].upper())
+print(bicycles[2].lower())
+print(bicycles[-1])
+print(bicycles[-2])
