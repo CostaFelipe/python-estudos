2847798e042a146c7cea2efa5532e539d7a49785
Felipe Costa <costadefelipe@gmail.com>
2021-02-25 14:01:01 -0300

    exercicios listas, primeiro

--- programas/book/03_listas.py
+++ programas/book/03_listas.py
@@ -15,3 +15,24 @@ print(bicycles[-2])
 #Usando valores individuais de uma lista
 message = "My first bicycle was a " + bicycles[2].title() + "."
 print(message)
+
+#exercicios listas
+#1)
+names = ['Ruth', 'Levi', 'Sueli','Raimundo', 'Diego', 'Thiago', 'Nilson', 'Felipe']
+print(names[0])
+print(names[-1])
+print(names[-2])
+print(names[1])
+print(names[2])
+print(names[-3])
+print(names[3])
+print(names[-4])
+
+#2)
+message = "I love so much " + names[2] + "!"
+print(message)
+
+#3)
+cars = ['bmw', 'troller', 'parati', 'citroen blue', 'Masserati']
+message = "Gostaria de ter o carro " + cars[0].title() + "."
+print(message)
