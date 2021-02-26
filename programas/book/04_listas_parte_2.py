fc362187be61566e2c1a4f9ac96adfffc836ef80
Felipe Costa <costadefelipe@gmail.com>
2021-02-26 14:59:47 -0300

    melhorando

--- programas/book/04_listas_parte_2.py
+++ programas/book/04_listas_parte_2.py
@@ -9,3 +9,4 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician.title() + ", that was great trick!")
     print("I can't wait to see the next trick" + magician.title() + "\n")
+print("Thank you, everyone. That was a great magic show!")
