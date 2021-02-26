25e703831a0982eaff974ccc8420243ef256fe4b
Felipe Costa <costadefelipe@gmail.com>
2021-02-26 14:58:13 -0300

    execuntado laços

--- programas/book/04_listas_parte_2.py
+++ programas/book/04_listas_parte_2.py
@@ -3,3 +3,9 @@ magicians = ['alice', 'david', 'carolina']
 
 for magician in magicians:
     print(magician)
+
+#Executando mais tarefas em um laco for
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was great trick!")
+    print("I can't wait to see the next trick" + magician.title() + "\n")
