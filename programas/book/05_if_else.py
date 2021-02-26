34fd4f463051f430b3cc0c103a85a9fe0951d926
Felipe Costa <costadefelipe@gmail.com>
2021-02-26 17:22:25 -0300

    and & or if

--- programas/book/05_if_else.py
+++ programas/book/05_if_else.py
@@ -21,3 +21,18 @@ age = 19
 print(age <= 21)
 print(age >= 21)
 print(age < 19)
+
+#Testando varias condicoes
+#and
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 and age_1 >= 21)
+
+age_1 = 22
+print(age_0 >= 21 and age_1 >= 21)
+
+#or
+age_1 = 18
+print(age_0 >= 21 or age_1 >= 21)
+age_0 = 18
+print(age_0 > 21 or age_1 >= 21)
