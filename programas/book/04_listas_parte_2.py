d1f71557b858a2276c1dcf19b659fc27288761bc
Felipe Costa <costadefelipe@gmail.com>
2021-02-26 15:46:08 -0300

    min(), max(), sum()

--- programas/book/04_listas_parte_2.py
+++ programas/book/04_listas_parte_2.py
@@ -50,3 +50,15 @@ for value in range(1, 11):
     square = value ** 2
     squares.append(square)
     print(squares)
+
+print("\n")
+
+for value in range(1,11):
+    squares.append(value ** 2)
+    print(squares)
+
+#Estatisticas simples com uma lista de numeros
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
