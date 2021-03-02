83e8b67c59c1f3d404a5e1a32370ea048186576f
Felipe Costa <costadefelipe@gmail.com>
2021-03-02 16:04:34 -0300

    Omitindo o bloco else

--- programas/book/05_if_else.py
+++ programas/book/05_if_else.py
@@ -171,3 +171,19 @@ if age < 4: price = 0
 elif age < 18: price = 5
 else: price = 10
 print("Your admission cost is $" + str(price) + ".")
+
+print("\n")
+#Usando varios blocos elif
+age = 64
+if age < 4: price = 0
+elif age < 18: price = 5
+elif age > 65: price = 5
+else: price = 10
+print("Your admission cost is $" + str(price) + ".")
+
+print("\n")
+age = 19
+if age < 4: price = 0
+elif age < 18: price = 5
+elif age < 65: price = 10
+elif age >= 65: price = 5
