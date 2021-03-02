87bfa514eeda01de4d530263040d621a69f9a66a
Felipe Costa <costadefelipe@gmail.com>
2021-03-02 14:56:12 -0300

    and if

--- programas/book/05_if_else.py
+++ programas/book/05_if_else.py
@@ -75,3 +75,10 @@ print(age <= 15)
 print(age >= 18)
 print(age > 15)
 print(age < 15)
+
+# and & or
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 and age_1 >= 21)
+age_1 = 22
+print(age_0 >= 21 and age_1 >= 21)
