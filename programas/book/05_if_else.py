a36945dc434a5a512243926174b34c2d20648562
Felipe Costa <costadefelipe@gmail.com>
2021-03-02 17:18:52 -0300

    Usando varias listas

--- programas/book/05_if_else.py
+++ programas/book/05_if_else.py
@@ -275,4 +275,17 @@ if requested_toppings:
         print("Adding " + requested_topping)
     print("\nFinished making your pizza!")
 else:
-    print("Are you sureyou want a plain pizza?")
+    print("Are you sure you want a plain pizza?")
+
+print("\n")
+#Usando varias listas
+avaiable_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni',
+                     'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in avaiable_toppings:
+        print("Adding " + requested_topping)
+    else:
+        print("Sorry, we dont have " + requested_topping)
+print("Finished making your pizza!!")
