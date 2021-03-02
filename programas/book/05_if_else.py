4a657a63bd55cc9da1a9d56018ee486770675928
Felipe Costa <costadefelipe@gmail.com>
2021-03-02 14:52:02 -0300

    <= >= < > if

--- programas/book/05_if_else.py
+++ programas/book/05_if_else.py
@@ -70,3 +70,8 @@ print(age == 18)
 answer = 17
 if answer != 42:
     print('That is not the correct answer. Please try again!')
+
+print(age <= 15)
+print(age >= 18)
+print(age > 15)
+print(age < 15)
