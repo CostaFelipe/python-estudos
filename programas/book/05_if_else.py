d347f131046395133c7aec2be3b6489d221f37ea
Felipe Costa <costadefelipe@gmail.com>
2021-03-02 15:15:41 -0300

    not in verificar lists

--- programas/book/05_if_else.py
+++ programas/book/05_if_else.py
@@ -94,3 +94,9 @@ print(age_0 >= 21 or age_1 >= 21)
 requested_toppings = ['mushrooms', 'onions', 'pineapple']
 print('mushrooms' in requested_toppings)
 print('pepperoni' in requested_toppings)
+
+#not in
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+if user not in banned_users:
+    print(user.title() + ", you can post a response if you wish.")
