3c37695048aa2b21e82b467002ad0f68eb72578f
Felipe Costa <costadefelipe@gmail.com>
2021-03-03 14:49:56 -0300

    exemplo simples de dicionário

--- programas/book/06_dicionarios.py
+++ programas/book/06_dicionarios.py
@@ -5,3 +5,11 @@ print(alien_0)
 #Acessando valores em um dicionario
 print(alien_0['color'])
 print(alien_0['points'])
+
+print("\n")
+#exemplo
+alien = {'color': 'green', 'points': 10}
+new_points = alien['points']
+print("You just earned " + str(new_points) + " points!")
+
+#Adicionando novos pares chave-valor
