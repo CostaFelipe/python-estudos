53533b021380c802f33731ba70e2f40ac5d7ec6a
Felipe Costa <costadefelipe@gmail.com>
2021-03-04 15:33:35 -0300

    Uma lista em um dicionario

--- programas/book/06_dicionarios.py
+++ programas/book/06_dicionarios.py
@@ -216,6 +216,9 @@ favorite_languages = { 'jen': ['python', 'ruby'], 'sarah': ['c'],
                        'edward': ['ruby', 'go'],
                        'phil': ['python', 'haskell'], }
 for name, languages in favorite_languages.items():
-    print("\n" + name.title() + "'s favorite languages are:")
+    print(name.title() + "'s favorite languages are:")
     for language in languages:
-        print("\t" + language.title())
+        print(language.title())
+
+#Um dicionario em um dicionario
+print('\nUm dicionario em um dicionario')
