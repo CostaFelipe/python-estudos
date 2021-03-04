b8a52bff0dec6aacd3bf191750a3a7d54784bb4a
Felipe Costa <costadefelipe@gmail.com>
2021-03-04 15:19:34 -0300

    exemplos lista c/dicionarios

--- programas/book/06_dicionarios.py
+++ programas/book/06_dicionarios.py
@@ -184,3 +184,23 @@ aliens = [alien_0, alien_1, alien_2]
 
 for alien in aliens:
     print(alien)
+
+print("\nCriar lista de dicionarios")
+aliens = []
+for alien in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:5]:
+    print(alien)
+
+print("total de alienigenas criados: " + str(len(aliens)))
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+
+for alien in aliens[:5]:
+    print(alien)
