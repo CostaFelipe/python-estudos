6697b9d4762e648e77a633debe2c4a516eaf9de5
Felipe Costa <costadefelipe@gmail.com>
2021-03-04 14:58:36 -0300

    Uma lista de dicionarios

--- programas/book/06_dicionarios.py
+++ programas/book/06_dicionarios.py
@@ -175,3 +175,12 @@ for name in friends:
         print("Ola " + name + ", obrigado por participar da enquente.")
     else:
         print("Ola " + name + ", voce deve participar da enquente.")
+
+print("\nUma lista de dicionarios")
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
