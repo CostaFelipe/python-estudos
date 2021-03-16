4fa05ab4c86aacda36244a62d74deecd422f7c02
Felipe Costa <costadefelipe@gmail.com>
2021-03-16 15:13:43 -0300

    Um dicionário em um dicionário

--- programas/book/08_dicionario.py
+++ programas/book/08_dicionario.py
@@ -1,1 +1,11 @@
+#coding=utf-8
 #Um dicionário em um dicionário
+users = {
+    'aeinstein': {
+                 'first': 'albert', 'last': 'einstein',
+                 'location': 'princeton', },
+    'felipe': {'first': 'lima', 'last': 'martins',
+                'location': 'sao paulo'}
+}
+for username, bio in users.items():
+    print(username.title() + " " + bio['first'])
