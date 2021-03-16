0cb8cd75a0e0be00ca086e59c09171d5f86703c6
Felipe Costa <costadefelipe@gmail.com>
2021-03-16 14:16:55 -0300

    dicionarios aninhados

--- programas/book/07_revisao.py
+++ programas/book/07_revisao.py
@@ -212,3 +212,12 @@ for language in favorite_languages.values():
 favorite_languages = {'felipe':'c', 'raquel':'c++', 'sarah':'c'}
 for language in set(favorite_languages.values()):
     print(language)
+
+##informações aninhadas
+alien_0 = {'color': 'yellow', 'points': 5}
+alien_1 = {'color': 'red', 'points': 10}
+alien_2 = {'color': 'orange','points': 15}
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
