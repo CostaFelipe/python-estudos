ae603d7d2741bf619383cb846e4b719c41394f34
Felipe Costa <costadefelipe@gmail.com>
2021-03-17 13:16:48 -0300

    dicionario sobre dicionarios

--- programas/book/08_dicionario.py
+++ programas/book/08_dicionario.py
@@ -5,7 +5,9 @@ users = {
                  'first': 'albert', 'last': 'einstein',
                  'location': 'princeton', },
     'felipe': {'first': 'lima', 'last': 'martins',
-                'location': 'sao paulo'}
+                'location': 'sao paulo'},
+    'ruth': {'first':'silva', 'last': 'gomes',
+              'location': 'santa catarina'}
 }
 for username, bio in users.items():
-    print(username.title() + " " + bio['first'])
+    print(username.title() + " " + bio['first'] + " " + bio['last'])
