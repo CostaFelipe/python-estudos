e1df11572091f5c6663529bf79cef9211f5c7e4e
Felipe Costa <costadefelipe@gmail.com>
2021-03-18 15:21:19 -0300

    Escrevendo prompts claros

--- programas/book/09_lacos-while_entradas.py
+++ programas/book/09_lacos-while_entradas.py
@@ -4,3 +4,12 @@
 #Como a função input() trabalha
 message = input("Tell me something, and I will repeat it back to you: ")
 print(message)
+
+#Escrevendo prompts claros
+name = input("Please enter your name:")
+print("Hello, " + name + "!")
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+name = input(prompt)
+print("\nHello, " + name)
