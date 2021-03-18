9a47358714ad4022c2479b6106882976fddb6be8
Felipe Costa <costadefelipe@gmail.com>
2021-03-18 15:46:44 -0300

    7.3

--- programas/book/09_lacos-while_entradas.py
+++ programas/book/09_lacos-while_entradas.py
@@ -11,3 +11,10 @@ if mesa >= 8:
     print("Por favor você deve esperar uma mesa")
 else:
     print("Sua mesa está pronta")
+
+multiplo_zhen = input("Informe um número para vê se é multiplo de 10: ")
+multiplo_zhen = int(multiplo_zhen)
+if multiplo_zhen % 10 == 0:
+    print(str(multiplo_zhen) + " é multiplo de 10")
+else:
+    print("O número " + str(multiplo_zhen) + "não é multiplo de 10" )
