bf14fe08d0fe0661115f40b1e473fc8d699aca0c
Felipe Costa <costadefelipe@gmail.com>
2021-03-18 15:40:05 -0300

    7.2

--- programas/book/09_lacos-while_entradas.py
+++ programas/book/09_lacos-while_entradas.py
@@ -4,3 +4,10 @@
 #exercicio
 locacao_carro = input("Qual tipo de carro que deseja alugar?")
 print("Deixe-me ver se consigo um " + locacao_carro + " para você.")
+
+mesa = input("Quantas pessoas estão em seu grupo para jantar?")
+mesa = int(mesa)
+if mesa >= 8:
+    print("Por favor você deve esperar uma mesa")
+else:
+    print("Sua mesa está pronta")
