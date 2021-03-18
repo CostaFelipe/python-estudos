b86736e822da943beb572d49f630d79390d7b30b
Felipe Costa <costadefelipe@gmail.com>
2021-03-18 16:16:26 -0300

    Usando uma flag

--- programas/book/10_lacos-while_entradas.py
+++ programas/book/10_lacos-while_entradas.py
@@ -15,3 +15,14 @@ while message != 'quit':
     message = input(prompt)
     if message != quit:
         print(message)
+
+#Usando uma flag
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+active = True
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
