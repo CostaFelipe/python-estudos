67e9b9014f5a1cdf30a8d626c817d7797317c2da
Felipe Costa <costadefelipe@gmail.com>
2021-03-19 14:13:29 -0300

    Removendo todas as instâncias de valores específicos de uma lista

---
+++ programas/book/11_lacos-while_entradas.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+#Usando um laço while com listas e dicionários
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+    print(confirmed_users)
+
+#Removendo todas as instâncias de valores específicos de uma lista
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
+    print(pets)
