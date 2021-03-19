9bfc997c31203c29c44460686042990dda89847a
Felipe Costa <costadefelipe@gmail.com>
2021-03-19 14:59:07 -0300

    7.9 – Sem pastrami:

--- programas/book/11_lacos-while_entradas.py
+++ programas/book/11_lacos-while_entradas.py
@@ -7,3 +7,14 @@ while sandwich_orders:
     print("preparei seu sanduíche " + current_sandwich)
     finished_orders.append(current_sandwich)
 print("esses são os sanduíche prontos:" + str(finished_orders))
+
+
+print("\n7.9 pastrami")
+sandwich_orders = ['paulistano', 'pastrami','bauru', 'x-burguer', 'pastrami', 'big-mac', 'pastrami']
+finished_orders = []
+print("a lanchonete está sem pastrami")
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+
+finished_orders.append(sandwich_orders)
+print(finished_orders)
