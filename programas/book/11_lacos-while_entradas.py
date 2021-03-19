397f79245a3d3aca65fdb39d98ab10d9ad2e07b4
Felipe Costa <costadefelipe@gmail.com>
2021-03-19 14:23:22 -0300

    reenchendo um dicionário com dados de entrada do usuário

--- programas/book/11_lacos-while_entradas.py
+++ programas/book/11_lacos-while_entradas.py
@@ -15,3 +15,17 @@ print(pets)
 while 'cat' in pets:
     pets.remove('cat')
     print(pets)
+
+#Preenchendo um dicionário com dados de entrada do usuário
+responses = {}
+polling_active = True
+while polling_active:
+    name = input("\nWhats is your name?")
+    response = input("Which mountain would you like to climb someday?")
+    responses[name] = response
+    repeat = input("Would you like to let another person respond? (yes/ no)")
+    if repeat == 'no':
+        polling_active =  False
+        print("\n--- Poll Results ---")
+        for name, response in responses.items():
+            print(name + " would like to climb " + response + ".")
