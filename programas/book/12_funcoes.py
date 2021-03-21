1ed4136df04e6540e6df025704688817d5860bfa
Felipe Costa <costadefelipe@gmail.com>
2021-03-21 15:10:25 -0300

    Valores default

--- programas/book/12_funcoes.py
+++ programas/book/12_funcoes.py
@@ -49,3 +49,10 @@ def describe_pets(animal_type, pet_name):
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 describe_pets(animal_type = 'dog', pet_name = 'aika')
+
+#Valores default
+def describe_pet(pet_name, animal_type='dog'):
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+describe_pet(pet_name='aika')
