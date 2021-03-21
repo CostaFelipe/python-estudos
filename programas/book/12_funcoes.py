81bd7aad85f3a8781ee632f8cbadfba24f54f0ee
Felipe Costa <costadefelipe@gmail.com>
2021-03-21 14:17:41 -0300

    Passando informações para uma função

--- programas/book/12_funcoes.py
+++ programas/book/12_funcoes.py
@@ -4,3 +4,10 @@ def greet_user():
     print("hello")
 
 greet_user()
+
+#Passando informações para uma função
+def greet_user(username):
+    """Exibe uma mensagem simples """
+    print("Hello " + username)
+
+greet_user('Felipe')
