1167f3599617a1c3af47566a3feba13a29853f41
Felipe Costa <costadefelipe@gmail.com>
2021-03-22 14:29:01 -0300

    Usando uma função com um laço while

--- programas/book/12_funcoes.py
+++ programas/book/12_funcoes.py
@@ -145,3 +145,15 @@ musician = build_person('jimi', 'hendrix', age=27)
 print(musician)
 musician = build_person('jimi', 'hendrix')
 print(musician)
+
+#Usando uma função com um laço while
+def get_formatted_name(first_name, last_name):
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+while True:
+    print("\nPlease tell me your name:")
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
+formatted_name = get_formatted_name(f_name, l_name)
+print("\nHello, " + formatted_name + "!")
