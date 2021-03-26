e1e8d2c51137b683772123f70954cba99acd85e7
Felipe Costa <costadefelipe@gmail.com>
2021-03-26 14:15:40 -0300

    Evitando que uma função modifique uma lista

--- programas/book/13_funcoes.py
+++ programas/book/13_funcoes.py
@@ -21,6 +21,7 @@ print("\nThe following models have been printed:")
 for completed_model in completed_models:
     print(completed_model)
 
+print("\n------------------------")
 #modificando o exemplo
 def print_models(unprinted_designs, completed_models):
     while unprinted_designs:
@@ -36,4 +37,12 @@ def show_completed_models(completed_models):
 unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
 completed_models = []
 print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
+
+#Evitando que uma função modifique uma lista
+print("\nparte 02 ------------------------------------")
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+print_models(unprinted_designs[:], completed_models)
+print("\nEvitando que uma função modifique uma lista")
 show_completed_models(completed_models)
