c33e6ffdc8e9bee060625e0d15a67b442e555883
Felipe Costa <costadefelipe@gmail.com>
2021-03-26 13:47:17 -0300

    lista sem função

--- programas/book/13_funcoes.py
+++ programas/book/13_funcoes.py
@@ -8,3 +8,15 @@ def greet_users(names):
 
 usernames = ['felipe', 'ruth', 'levi']
 greet_users(usernames)
+
+#Modificando uma lista em uma função
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print("Printing model: " + current_design)
+    completed_models.append(current_design)
+
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model)
