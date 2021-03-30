4282a982d20c2c4885a454093d333cb543ff2857
Felipe Costa <costadefelipe@gmail.com>
2021-03-30 14:18:25 -0300

    Importando todas as funcoes de um modulo

--- programas/book/14_modules/making_pizzas.py
+++ programas/book/14_modules/making_pizzas.py
@@ -8,6 +8,12 @@ from pizza import make_pizza as mp
 #Usando a palavra reservada as para atribuir um alias a um modulo
 import pizza as p
 
+#Importando todas as funcoes de um modulo
+from pizza import *
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
 p.make_pizza(12, 'peppori')
 
 pizza.make_pizza(12, 'peppori')
