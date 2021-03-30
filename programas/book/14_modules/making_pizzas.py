25f21a255d2850ffc208229c51b939d24019b71e
Felipe Costa <costadefelipe@gmail.com>
2021-03-30 14:05:16 -0300

    Importando funções específicas

--- programas/book/14_modules/making_pizzas.py
+++ programas/book/14_modules/making_pizzas.py
@@ -1,4 +1,4 @@
-import pizza
+from pizza import make_pizza
 
-pizza.make_pizza(12, 'pepperoni')
-pizza.make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
+make_pizza(12, 'pepperoni')
+make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
