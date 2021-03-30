9d27a1e2401eded245380ee4d8fd2cbcfead899f
Felipe Costa <costadefelipe@gmail.com>
2021-03-30 15:37:05 -0300

    Criando várias instâncias

--- programas/book/15_classes.py
+++ programas/book/15_classes.py
@@ -21,3 +21,15 @@ print("My dog is " + str(my_dog.age) + " years old.")
 my_dog = Dog('willie', 6)
 my_dog.sit()
 my_dog.rool_over()
+
+#Criando várias instâncias
+my_dog = Dog('willie', 6)
+your_dog = Dog('lucy', 3)
+
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is "+ str(my_dog.age) + " years old.")
+my_dog.sit()
+
+print("\nYour dog's name is " + your_dog.name.title() + ".")
+print("Your dog is " + str(your_dog.age) + " years old.")
+your_dog.sit()
