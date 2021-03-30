046ae5c0f1c2a1d5ba0d047027922dc7d18f0f39
Felipe Costa <costadefelipe@gmail.com>
2021-03-30 14:32:28 -0300

    8.16 – Importações:

--- programas/book/14_modules/print_models.py
+++ programas/book/14_modules/print_models.py
@@ -1,5 +1,25 @@
 import printing_functions as p
+import printing_functions
+from printing_functions import *
+from printing_functions import print_models as md
 
 unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
 completed_models = []
+
 p.print_models(unprinted_designs, completed_models)
+
+print("\n")
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+printing_functions.print_models(unprinted_designs, completed_models)
+
+print("\n")
+unprinted_designs = ['iphone', 'robot', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+
+print("\n")
+unprinted_designs = ['iphone', 'robot', 'dodecahedron']
+completed_models = []
+md(unprinted_designs, completed_models)
