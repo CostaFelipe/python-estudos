eea48f013f303eba67e869e4d3c9ca41f8821e49
Felipe Costa <costadefelipe@gmail.com>
2021-03-30 14:11:10 -0300

    palavra reservada

--- programas/book/14_modules/making_pizzas.py
+++ programas/book/14_modules/making_pizzas.py
@@ -1,4 +1,8 @@
 from pizza import make_pizza
+#palavra reservada
+from pizza import make_pizza as mp
 
 make_pizza(12, 'pepperoni')
 make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
+
+mp(12, 'pepperoni')
