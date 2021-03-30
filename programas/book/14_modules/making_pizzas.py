3126d4e83dc174a57599979a3047fe2277225f93
Felipe Costa <costadefelipe@gmail.com>
2021-03-30 14:12:54 -0300

    importando total o modulo

--- programas/book/14_modules/making_pizzas.py
+++ programas/book/14_modules/making_pizzas.py
@@ -1,7 +1,11 @@
+#importando total o modulo
+import pizza
 #importando funcoes especifica
 from pizza import make_pizza
 #palavra reservada
 from pizza import make_pizza as mp
+
+pizza.make_pizza(12, 'peppori')
 
 make_pizza(12, 'pepperoni')
 make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
