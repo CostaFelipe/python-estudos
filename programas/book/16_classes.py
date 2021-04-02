9d86b8823ad355aceebbca592b56d3b2bfdce14a
Felipe Costa <costadefelipe@gmail.com>
2021-04-02 15:06:56 -0300

    Modificando o valor de um atributo com um método

--- programas/book/16_classes.py
+++ programas/book/16_classes.py
@@ -23,6 +23,8 @@ class Car():
         return long_name.title()
     def read_odometer(self):
         print("This car has " + str(self.odometer_reading) + " miles on it.")
+    def update_odometer(self, mileage):
+        self.odometer_reading = mileage
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
@@ -33,4 +35,8 @@ my_new_car.read_odometer()
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
+#Modificando o valor de um atributo com um método
+my_new_car.update_odometer(34)
 my_new_car.read_odometer()
