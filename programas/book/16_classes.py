5b3a63a8c9528dd557e83097c98837a1b476b93a
Felipe Costa <costadefelipe@gmail.com>
2021-04-02 15:02:32 -0300

    Modificando o valor de um atributo diretamente

--- programas/book/16_classes.py
+++ programas/book/16_classes.py
@@ -27,3 +27,10 @@ class Car():
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
+
+#Modificando valores de atributos
+#Modificando o valor de um atributo diretamente
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
