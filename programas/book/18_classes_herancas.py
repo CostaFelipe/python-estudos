d999af0eeb318d85c655a83a5a75eb77078d5ade
Felipe Costa <costadefelipe@gmail.com>
2021-04-06 14:17:49 -0300

    classe-filha de Car()

--- programas/book/18_classes_herancas.py
+++ programas/book/18_classes_herancas.py
@@ -17,3 +17,10 @@ class Car():
         else: print("You can't roll back an odometer!")
     def increment_odometer(self, miles):
         self.odometer_reading += miles
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
