aaeeea9073d2bef8fcd6b1c7f39a64d85ddd43b6
Felipe Costa <costadefelipe@gmail.com>
2021-04-20 14:33:38 -0300

    acrescentando class Battery() in module car.py

--- programas/book/20_classes/car.py
+++ programas/book/20_classes/car.py
@@ -16,3 +16,17 @@ class Car():
             print("You can't rool back an odometer")
     def increment_odometer(self, miles):
         self.odometer_reading += miles
+
+class Battery():
+    def __init__(self, battery_size = 60):
+        self.battery_size = battery_size
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+    def get_range(self):
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
