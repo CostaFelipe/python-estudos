851ec254a3e6a81131f22c375f3798d39435808a
Felipe Costa <costadefelipe@gmail.com>
2021-04-20 14:56:42 -0300

    call method and import ElectricCar in module my_electricCar.py

--- programas/book/20_classes/car.py
+++ programas/book/20_classes/car.py
@@ -18,7 +18,7 @@ class Car():
         self.odometer_reading += miles
 
 class Battery():
-    def __init__(self, battery_size = 60):
+    def __init__(self, battery_size = 70):
         self.battery_size = battery_size
     def describe_battery(self):
         print("This car has a " + str(self.battery_size) + "-kWh battery.")
@@ -33,5 +33,5 @@ class Battery():
 
 class ElectricCar(Car):
     def __init__(self, make, model, year):
-        super.__init__(make, model, year)
+        super().__init__(make, model, year)
         self.battery = Battery()
