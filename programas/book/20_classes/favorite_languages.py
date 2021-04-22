f6dd8465586c38fdc29a7c0de1b3a7d2976a2c71
Felipe Costa <costadefelipe@gmail.com>
2021-04-22 15:30:57 -0300

    dicionario

--- programas/book/20_classes/favorite_languages.py
+++ programas/book/20_classes/favorite_languages.py
@@ -9,3 +9,7 @@ favorite_languages['phil'] = 'python'
 
 for name, language in favorite_languages.items():
     print(name.title() + "'s favorite language is " + language.title() + ".")
+
+#relembrando
+user = {'nome':'felipe'}
+print(user)
