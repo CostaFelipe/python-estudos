3b67d8de21bf8c97a3009dc7063deb793877ef96
Felipe Costa <costadefelipe@gmail.com>
2021-04-22 14:16:25 -0300

    delete files

--- programas/book/20_classes/empresa.py
+++
@@ -1,7 +0,0 @@
-from endereco import Address
-
-class Empresa():
-    def __init__(self, nome, cnpj):
-        self.nome = nome
-        self.cnpj = cnpj
-        self.address = Address()
--- programas/book/20_classes/endereco.py
+++
@@ -1,4 +0,0 @@
-class Address():
-    def __init__(self, rua, referencia):
-        self.rua = rua
-        self.referencia = referencia
