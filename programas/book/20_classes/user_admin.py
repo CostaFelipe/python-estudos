e0c8c36f8d1cce87c693e8d1025263bca743a89d
Felipe Costa <costadefelipe@gmail.com>
2021-04-22 15:23:51 -0300

    9.11 – Importando Admin

---
+++ programas/book/20_classes/user_admin.py
@@ -0,0 +1,12 @@
+from user import Admin
+
+eric = Admin('eric', 'matthes', 'e_matthes', 'e_matthes@example.com', 'alaska')
+eric.describe_user()
+
+eric.privileges = [
+    'can reset passwords',
+    'can moderate discussions',
+    'can suspend accounts',
+    ]
+
+eric.show_privileges()
