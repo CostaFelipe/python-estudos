0861f7f8fdeccf7002c80f4d6f9400970ea50f95
Felipe Costa <costadefelipe@gmail.com>
2021-05-04 14:31:49 -0300

    5 operacoes

--- programas/book/revisao/01_revisao.py
+++ programas/book/revisao/01_revisao.py
@@ -10,3 +10,11 @@ print(message.strip())
 #concatenacao
 message += " " + 'Brasil'
 print(message)
+
+#operacoes
+print(1 + 1)
+print(1.5 + 1)
+print(2**2)
+print(2 * 3)
+print(10 / 5)
+print(10 % 2)
