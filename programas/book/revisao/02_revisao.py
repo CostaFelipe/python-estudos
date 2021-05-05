f963c067136aa88b29cb3d36113b1c0a776d00c1
Felipe Costa <costadefelipe@gmail.com>
2021-05-05 14:44:11 -0300

    python version

--- programas/book/revisao/02_revisao.py
+++ programas/book/revisao/02_revisao.py
@@ -5,3 +5,7 @@ for i in n:
 print(soma)
 
 print("Twinkle, twinkle, little start, \n\t How I wonder what you are! \n\t\t Up abovethe world so high, \n\t\t Like a diamond in the sky. \nTwinkle, twinkle, little star, \n\t How I wonder what you are")
+
+import sys
+print("Python version")
+print (sys.version)
