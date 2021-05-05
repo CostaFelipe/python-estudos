002cf10b25298894c8c180b9277a395ca529adf1
Felipe Costa <costadefelipe@gmail.com>
2021-05-05 14:48:07 -0300

    Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them

--- programas/book/revisao/02_revisao.py
+++ programas/book/revisao/02_revisao.py
@@ -19,3 +19,8 @@ print(now)
 
 r = float(input ("Input the radius of the circle : "))
 print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print ("Hello  " + lname + " " + fname)
