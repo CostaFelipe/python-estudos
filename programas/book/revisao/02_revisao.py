710f6188e3896059df41b5f9ad76b412f897f7c4
Felipe Costa <costadefelipe@gmail.com>
2021-05-05 14:46:55 -0300

    Write a Python program which accepts the radius of a circle from the user and compute the area.

--- programas/book/revisao/02_revisao.py
+++ programas/book/revisao/02_revisao.py
@@ -1,3 +1,7 @@
+import sys
+import datetime
+from math import pi
+
 n = [1, 1, 1, 4]
 soma = 0
 for i in n:
@@ -6,10 +10,12 @@ print(soma)
 
 print("Twinkle, twinkle, little start, \n\t How I wonder what you are! \n\t\t Up abovethe world so high, \n\t\t Like a diamond in the sky. \nTwinkle, twinkle, little star, \n\t How I wonder what you are")
 
-import sys
+
 print("Python version")
 print (sys.version)
 
-import datetime
 now = datetime.datetime.now()
 print(now)
+
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
