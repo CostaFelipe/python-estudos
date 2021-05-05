f3e14a477d9c5c39d2dd7790fec159287413fe77
Felipe Costa <costadefelipe@gmail.com>
2021-05-05 14:42:21 -0300

    1. Write a Python program to print the following string in a specific format (see the output). Go to the editor Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" Output :

--- programas/book/revisao/02_revisao.py
+++ programas/book/revisao/02_revisao.py
@@ -3,3 +3,5 @@ soma = 0
 for i in n:
     soma = soma + i
 print(soma)
+
+print("Twinkle, twinkle, little start, \n\t How I wonder what you are! \n\t\t Up abovethe world so high, \n\t\t Like a diamond in the sky. \nTwinkle, twinkle, little star, \n\t How I wonder what you are")
